c74d70e4630e44766d9244186eafdc456174e1fc
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-09-29 19:22:20 +0200

    finish crud fro poducts
    issues with get by id

--- src/services/products/routes.js
+++ src/services/products/routes.js
@@ -13,7 +13,7 @@ const { Op } = s
 router.get("/", async (req, res, next) => {
     try {
         const data = await Product.findAll({
-            attributes: {exclude: ["id", "createdAt"]},
+            attributes: {exclude: ["createdAt"]}, // "id"
             include: Review
         })
         res.send(data)
@@ -38,6 +38,62 @@ router.post("/", productValidation, async (req, res, next) => {
         console.log(error)
         next(error)  
         }
+    }
+})
+
+router.get("/:productId", async (req, res, next) => {
+    try {
+        // const data = await Product.findAll({
+        //     where: { id: req.params.productId}
+        // })
+
+        // if(data){
+        //     res.send(data)
+        // } else {
+        //     next(createHttpError(404, "No product with that id"))
+        // }
+
+        const data = await Product.findAll({
+            where: { id: req.params.productId },
+            include: Review
+        })
+        res.send(data)
+        
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
+
+router.put("/:productId", async (req, res, next) => {
+    try {
+        const data = await Product.update(req.body, {
+            where: { id: req.params.productId },
+            returning: true
+        })
+
+        res.send(data[1][0])
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
+
+router.delete("/:productId", async (req, res, next) => {
+    try {
+        const rows = await Product.destroy({
+            where: { id: req.params.productId }
+        })
+
+        if(rows > 0){
+            res.send(204)
+
+        } else {
+            next(createHttpError(404), "Product not found")
+        }
+    } catch (error) {
+        console.log(error)
+        next(error)
     }
 })
 
