1d5ca93d3c24c2fed222cff5f271e53b5ec49d38
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-09-29 19:47:41 +0200

    finish review crud

--- src/server.js
+++ src/server.js
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 import { connectDB, testDB } from "./db/index.js";
 import { badRequest, forbidden, notFound, serverError } from "./errorHandlers.js";
-// import db from "./db/models/models.js"
 import productsRouter from "./services/products/routes.js"
+import reviewsRouter from "./services/reviews/routes.js"
+// import db from "./db/models/models.js"
 
 const server = express()
 
@@ -13,6 +14,7 @@ server.use(cors())
 server.use(express.json())
 
 server.use("/products", productsRouter)
+server.use("/reviews", reviewsRouter)
 
 server.use(badRequest)
 server.use(forbidden)
